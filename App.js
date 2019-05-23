f6c2d36dbdb33109ac7d405a2a40e8df4c139365
solcream <47736779+solcream@users.noreply.github.com>
2019-05-23 10:09:34 +0200

    init commit

--- App.js
+++ App.js
@@ -1,52 +1,53 @@
-import React, { Component } from 'react';
-import './App.css';
-import Nyhetslista from '../src/components/Nyhetslista';
- 
-class App extends Component {
-  constructor(props) {
-  super(props);
-  this.state = {
-    articles: [
-  {
-  urlTolmage: "https://source.unsplash.com/random/200*125/?furniture",
-  title:  "Testnyhet 1",
-  description: "Beskrivning av testnyheten",
-  },
-  {
-  urlTolmage: "https://source.unsplash.com/random/200*125/?furniture",
-title: "Testnyhet 2",
- description: "Beskrivning av den andra testnyheten",
-  }]};
-}
+import React , {component} from 'react';
+import logo from './logo.svg ' ;
+import "./App.css ";
+import Nyhetslista  from './components/Nyhetslista';
 
-componentDidMount() {
-    fetch ( " https://newsapi.org/v2/top-headlines?country=se&apiKey=f7d96f9fff3445bf9d132c9b4dba09aa " )
-    .then( function (response) {
-        // gör något med det som kom tillbaka 
-    if ( response.status ==! 200) {
-        throw Error( `status: ${response.status}`);
-      }
-     return response.json()
-} ).then( jsondata => {
-  // gör något med json-objektet
-  this.setState({ articles: jsondata.articles  })
-}).catch(error => {
-  this.setState({
-    articles: [{
-      urlTomage: "fejk.jpg",
-      titel: "Något gick fel",
-      description: "Något gick fel. $(error.message)",
-    }]
-  });
-})
-}
+class  App  extends  component {
+constructor(props) {
+super (props);
+    this.state = {
+      articles: [
+        {
+          urlToImage :  " https://source.unsplash.com/random/200×125/?cat ",
+    titel : "Testnyhet 1",
+   beskrivning : " Beskrivning av testnyheten ",
+    },
+      {
+        urlToImage :  " https://source.unsplash.com/random/200×125/?cat ",
+   titel : "Testnyhet 2",
+  beskrivning : " Beskrivning av den andra testnyheten ",
+  }]}
+  }
 
+  componentDidMount () {
+    fetch ("https://newsapi.org/v2/top-headlines?country=se&apiKey=ae24bb6763034855bf26908e36dcd782 " )
+      .then(function(response) {
+      // gör något med det som kom tillbaka
+        if(response.status ==! 200) {
+          throw Error(`state: ${response.status}`)
+        }
+        return response.json()
+    } ).then(jsondata => {
+      // gör något med json-objektet
+      this.setState({articles: jsondata.articles})
+    } ).catch (error => {
+    this.setState ({  
+        artiklar: [{
+          urlToImage:   "fejk.jpg " ,
+          titel:  " Något gick fel " ,
+          description:  `Något gick fel, $ {error.message}`,
+ }]
+      });
+    })
+  }
+  
 render() {
   return (
-  <Nyhetslista
-  minArtiklar ={this.state.articles}/>
-  );
-}
+      <Nyhetslista
+        minaArtiklar = {this.state.articles} / >
+    );
+  }
 }
 
 export default App;
--- App.test.js
+++ App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import app from './App';
 
-it('renders without crashing', () => {
+('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
